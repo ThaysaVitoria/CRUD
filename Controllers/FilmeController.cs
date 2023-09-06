45c2eedb4b8d0698357f864f810da3a2fe7648ee
ThaysaVitoria <thaysavitoria@gmail.com>
2023-09-06 14:15:35 -0300

    Subindo para o github

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -22,5 +22,77 @@ namespace Cinema.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Cadastrar(FilmeModel filme)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Filmes.Add(filme);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            FilmeModel filme = _db.Filmes.FirstOrDefault(x => x.Id == id);
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+
+            return View(filme);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(FilmeModel filme)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Filmes.Update(filme);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(filme);
+        }
+        [HttpGet]
+
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            FilmeModel filme = _db.Filmes.FirstOrDefault(x => x.Id == id);
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            return View(filme);
+        }
+        [HttpPost]
+        public IActionResult Excluir(FilmeModel filme)
+        {
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _db.Filmes.Remove(filme);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/FilmeModel.cs
+++ Models/FilmeModel.cs
@@ -3,9 +3,14 @@
     public class FilmeModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o Titulo")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "Digite a Produtora")]
         public string Produtora { get; set; }
+        [Required(ErrorMessage = "Digite o Descricao")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Digite a Estreia")]
         public DateTime Estreia { get; set; } = DateTime.Now;
     }
 }
